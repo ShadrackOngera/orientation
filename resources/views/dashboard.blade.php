e9d06eafb85f787046f3ff0fa7a23da8421e526f
shadrackongera <shadrack.sme@gmail.com>
2023-03-20 00:21:28 +0300

    dashboard accordion

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -34,8 +34,51 @@
             </svg>
         </span>
         <a href="{{ route('modules.sports') }}" class="btn btn-outline-primary">Sports</a>
-
-
+    </div>
+    <div class="py-5"></div>
+    <div class="accordion" id="accordionExample">
+        <h2>Frequently asked questions</h2>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    How To Navigate through the system
+                </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam beatae cumque dignissimos eaque impedit itaque laborum quod sint suscipit veniam.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius nisi quaerat quisquam sequi voluptatem? Dolores eum ex excepturi fugiat hic! Aspernatur cupiditate esse fugit nam perferendis quasi qui. Cupiditate dicta eius laborum, neque non praesentium quaerat quo soluta totam. A ad amet assumenda beatae consequuntur corporis culpa deleniti ea, eaque est excepturi in laborum natus nemo perspiciatis repellendus sapiente sint sunt, suscipit tenetur totam vero voluptatum! Alias consequatur eveniet ex fugit hic maxime omnis, placeat quidem sapiente velit voluptate voluptates.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Accordion Item #2
+                </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Accordion Item #3
+                </button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
