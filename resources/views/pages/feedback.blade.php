973ae918f603c4275abfe6432d9d494e37359bda
shadrackongera <shadrack.sme@gmail.com>
2023-03-27 09:43:53 +0300

    feedback page

--- resources/views/pages/feedback.blade.php
+++ resources/views/pages/feedback.blade.php
@@ -4,12 +4,34 @@
         <h3>Welcome to Our Feedback page</h3>
         <form action="">
             @csrf
-            <div class="form-floating">
+            <div>
+                <label for="floatingInput" class="text-muted">How Comfortable are you now visiting the school</label>
+                <select class="form-select mb-3 py-3" aria-label="Default select example" name="comfort" required>
+                    <option value="1">Very Uncomfortable</option>
+                    <option value="2">Somewhat Uncomfortable</option>
+                    <option value="3">unsure</option>
+                    <option value="4">Somewhat comfortable</option>
+                    <option value="5">very comfortable</option>
+                </select>
+            </div>
+
+            <div>
+                <label for="floatingInput" class="text-muted">How Likely are you to recommend our system</label>
+                <select class="form-select mb-3 py-3" aria-label="Default select example" name="likely" required>
+                    <option value="1">Very Unlikely</option>
+                    <option value="2">Somewhat Unlikely</option>
+                    <option value="3">unsure</option>
+                    <option value="4">Somewhat likely</option>
+                    <option value="5">very likely</option>
+                </select>
+            </div>
+
+            <div class="form-floating mb-3">
                 <textarea class="form-control" placeholder="Leave your FeedBack here" name="content" id="floatingTextarea2" style="height: 100px"></textarea>
-                <label for="floatingTextarea2">Message</label>
+                <label for="floatingTextarea2">Leave Your Feedback here</label>
             </div>
             <div class="col-sm-5 d-grid float-end">
-                <button type="submit" class="btn btn-outline-primary">
+                <button type="submit" class="btn btn-outline-info">
                     Send
                 </button>
             </div>
